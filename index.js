acebcd9c4bfff765ab6526c380ac96b0806a087c
SpoonMcForky <55011606+SpoonMcForky@users.noreply.github.com>
2022-08-04 09:00:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { messages } = require('powercord/webpack');
+
+module.exports = class RemoveTracking extends Plugin {
+  startPlugin() {
+    this.patchMessage();
+  }
+  patchMessage() {
+    inject('removeTracking', messages, 'sendMessage', args => {
+      if (args[1].content.search('https://open.spotify.com/track') !== -1) args[1].content = args[1].content.replace(/\?.{19}/, '');
+      else if (args[1].content.search('https://twitter.com') !== -1) {
+        args[1].content = args[1].content.replace(/\?.+/, '');
+        //https://www.tiktok.com/@richardsalesofficial/video/7127773315233598725?is_from_webapp=1&sender_device=pc
+        args[1].content = args[1].content.replace(/https:\/\/twitter\.com/, 'https://vxtwitter.com');
+      } else if (args[1].content.search('https://www.tiktok.com/') !== -1) {
+        args[1].content = args[1].content.replace(/\?.+/, '');
+        args[1].content = `https://tt-embed.com/?q=${ args[1].content}`;
+      }
+      console.log(args[1].content);
+      return args;
+    }, true);
+  }
+  pluginWillUnload() {
+    uninject('removeTracking'); powercord.api.settings.unregisterSettings(this.entityID);
+  }
+};
