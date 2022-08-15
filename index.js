8e6a2095f847307878479564c6fb38c0b730e346
SpoonMcForky <55011606+SpoonMcForky@users.noreply.github.com>
2022-08-15 00:43:22 -0400

    Added settings

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
 const { messages } = require('powercord/webpack');
 
+const Settings = require('./components/settings.jsx');
+
 module.exports = class RemoveTracking extends Plugin {
   startPlugin() {
+    this.setDefault('vx', false);
+    powercord.api.settings.registerSettings(this.entityID, {
+      category: this.entityID,
+      label: 'Remove Trackers',
+      render: Settings
+    });
     this.patchMessage();
+  }
+  setDefault(name, defaultValue) {
+    this.settings.set(name, this.settings.get(name, defaultValue));
   }
   patchMessage() {
     inject('removeTracking', messages, 'sendMessage', args => {
@@ -12,7 +23,7 @@ module.exports = class RemoveTracking extends Plugin {
       if (x[1].content.search('https://open.spotify.com/track') !== -1) x[1].content = x[1].content.replace(/\?.[^\s]*/g, () => '');
       else if (x[1].content.search('https://twitter.com') !== -1) {
         x[1].content = x[1].content.replace(/\?.[^\s]*/g, () => '');
-        x[1].content = x[1].content.replace(/https:\/\/twitter\.com/g, () => 'https://vxtwitter.com');
+        if (this.settings.get('vx')) x[1].content = x[1].content.replace(/https:\/\/twitter\.com/g, () => 'https://vxtwitter.com');
       } else if (x[1].content.search('https://www.tiktok.com/') !== -1) {
         x[1].content = x[1].content.replace(/\?.[^\s]*/g, () => '');
         x[1].content = x[1].content.replace(/https:\/\/www\.tiktok\.com/g, () => `https://tt-embed.com/?q=${x[1].content}`);
